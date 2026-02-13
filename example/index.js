735648eec525dd50f8b241d2ce0cd55303bb22ef
David <davidham975@gmail.com>
2026-02-13 20:51:53 +1100

    extended example with increment and decrement

--- example/index.js
+++ example/index.js
@@ -7,16 +7,22 @@ const App = () => {
   element.className = "p-5 h-screen";
 
   const btn = document.createElement("button");
-  btn.className = "btn btn-square btn-success";
-  btn.onclick = () => setVal((prev) => prev + 1);
-  btn.textContent = "+";
+  btn.className = "btn btn-square btn-error";
+  btn.onclick = () => setVal((prev) => prev - 1);
+  btn.textContent = "-";
 
   const displayValue = document.createElement("p");
-  displayValue.className = "input w-9 ml-1";
+  displayValue.className = "input w-10 mx-1 text-center";
   displayValue.textContent = String(val());
+
+  const btn2 = document.createElement("button");
+  btn2.className = "btn btn-square btn-success";
+  btn2.onclick = () => setVal((prev) => prev + 1);
+  btn2.textContent = "+";
 
   element.appendChild(btn);
   element.appendChild(displayValue);
+  element.appendChild(btn2);
 
   effect(() => {
     // reactive context, will set textContent whenever we call setVal (the button does that)
